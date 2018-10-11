ce6545b899d91a10995c9211a33b4c2ee7398c9b
kyungyunlee <kyungyunlee2393@gmail.com>
2018-10-11 19:01:54 +0900

    add overlay for audio selection

--- assets/project2/sketch.js
+++ assets/project2/sketch.js
@@ -1,9 +1,11 @@
-// 2 ways of analyzing music
-// 1. from local mp3
-// 2. from mic input 
+// 3 ways of analyzing music
+// 1. from server mp3
+// 2. from local mp3
+// 3. from youtube/mic input 
 
 var audio;
-// 1, from local mp3 
+var currently_playing;
+// 1, from server mp3 
 var song;
 var button; // play mp3 song button
 var fft, amplitude, level;
@@ -11,7 +13,13 @@ var mysongs = ['vulfpeck_birds_of_a_feather.mp3', 'vulfpeck_animalspirit.mp3','v
 // var mysongs = ['https://www.youtube.com/embed/qTUnDV3MgVQ']
 var sel; 
 
-// 2. from mic input 
+// 2. from local mp3
+var usersong;
+var usersonglink;
+var usersongButton;
+var is_user_song = false;
+
+// 3. from mic input 
 var mic;
 var micbutton; // mic input on/off button
 var is_mic_on = false;
@@ -20,6 +28,7 @@ var youtubelink;
 var submitlink;
 var curr_link;
 var is_youtube_on = false;
+
 
 // text parameters
 var num_letters = 15;
@@ -97,6 +106,15 @@ function setup(){
     micbutton = createButton("use mic");
     micbutton.mousePressed(micTogglePlaying);
     micbutton.parent("mic");
+
+    // user song input button
+    usersonglink = createFileInput(handleFile);
+    usersonglink.parent("user-mp3");
+    usersonglink.changed(getUserFile);
+    usersongButton = createButton("play");
+    usersongButton.parent("user-mp3");
+    usersongButton.mousePressed(toggleUserSongPlaying);
+    
 
     // set base heights for four letters 
     startingheight = windowHeight - energy_threshold;
@@ -191,7 +209,7 @@ function draw(){
 
     textFont("Arial Black");
     for (var c=num_letters-1; c>=0;c--){
-        textSize(base_fontsize + (c*12));
+        textSize(base_fontsize + (c*11));
         for (var w=0; w<words.length;w++){
             fill (letter_colors[w]);
             var diff_h = map(ypos[w][num_letters-1-c], energy_threshold, 255, energy_threshold, windowHeight /4);
@@ -266,14 +284,28 @@ function closeYoutube(){
     is_youtube_on = false;
 }
 
+/***** functions for handling user's local mp3 file *****/
+function handleFile(file){
+    console.log(file);
+    usersong = loadSound(file, successCallback, errorCallback, whileLoading);
+    fft.setInput(usersong);
+    amplitude.setInput(usersong);
+}
+
+function getUserFile(){
+    is_user_song = true;
+    usersongButton.html("loading..please wait")
+}
+
 /***** functions for handling and loading new sound file  *****/
 
 function mySelectEvent (){
     if (song && song.isPlaying()){
-        song.stop();
-        curr_segment=0;
+        stopmp3();
+    } else if (usersong && usersong.isPlaying()){
+        stopUserMp3();
     }
-    button.html("loading..please wait");
+    button.html("loading..please wait")
     song_name = sel.value();
     song = loadSound('assets/project2/' + song_name, successCallback, errorCallback, whileLoading);
 
@@ -284,7 +316,11 @@ function mySelectEvent (){
 
 
 function successCallback(){
-    button.html("play!");
+    if (is_user_song) { 
+        usersongButton.html("play!");
+    } else { 
+        button.html("play!");
+    }
     console.log("loaded");
 }
 
@@ -294,6 +330,7 @@ function errorCallback(){}
 function whileLoading(total){
     console.log(total);
     button.html("loading.." + nf(total*100,2,0)+"%");
+
 }
 
 
@@ -301,8 +338,12 @@ function whileLoading(total){
 function stopmp3(){
     song.stop();
     console.log("Song stopped");
-    curr_segment = 0;
     button.html("play!");
+
+
+    var currentAudioTitle = select("#currentAudioTitle");
+    currentAudioTitle.remove();
+
 }
 
 function playmp3(){
@@ -310,10 +351,35 @@ function playmp3(){
     amplitude.setInput(song);
 
     song.play();
-    console.log("Song playing");
-    curr_segment=0;
+    console.log("Song playing " + song.url);
     button.html("pause");
+    
+    var currentAudioTitle = createP(song.url);
+    currentAudioTitle.attribute("id", "currentAudioTitle");
+    var placeholder = select("#current-audio-file");
+    currentAudioTitle.parent(placeholder);
+}
 
+function playUserMp3(){
+    fft.setInput(usersong);
+    amplitude.setInput(usersong);
+
+    usersong.play();
+    console.log("User Song playing");
+    usersongButton.html("pause");
+
+    is_user_song = true;
+
+    var currentAudioTitle = createP(usersong.url);
+    var placeholder = select("#current-audio-file");
+    currentAudioTitle.parent(placeholder);
+}
+
+function stopUserMp3(){
+    usersong.stop();
+    console.log("User Song stopped");
+    usersongButton.html("play!");
+    is_user_song = false;
 }
 
 function startmic(){
@@ -337,14 +403,25 @@ function togglePlaying(){
     // turn off youtube, mic if they were on 
     if (is_youtube_on){ closeYoutube(); }
     if (is_mic_on){ stopmic();}
+    if (usersong && usersong.isPlaying()) { stopUserMp3(); }
     
     if (song.isPlaying()){ stopmp3(); } 
     else { playmp3(); }
 }
 
+function toggleUserSongPlaying(){
+    if (is_youtube_on) { closeYoutube(); }
+    if (is_mic_on) { stopmic(); }
+    if (song.isPlaying()) { stopmp3(); }
+
+    if (usersong.isPlaying()) { stopUserMp3(); }
+    else { playUserMp3(); }
+}
+
 function micTogglePlaying(){
     // turn of youtube, mp3 if they were on
     if (song.isPlaying()){ stopmp3();}
+    if (usersong && usersong.isPlaying()) { stopUserMp3(); }
     if (is_youtube_on){ closeYoutube(); } 
 
     if (is_mic_on){ stopmic();} 
